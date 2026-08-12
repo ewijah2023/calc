3a783f0623b882462e28123015fc357fddd0b2f4
ewijah <edgordon2023@gmail.com>
2026-08-12 11:13:13 -0400

    add math functions

---
+++ main/main.js
@@ -0,0 +1,24 @@
+let num1 = 0;
+let num2 = 0;
+let operator;
+
+function add(a, b){
+    let sum = a + b;
+    return sum;
+
+}
+
+function subtract(a, b) {
+    let difference = a - b;
+    return difference;
+}
+
+function multiply(a, b){
+    let product = a * b;
+    return product;
+}
+
+function divide(a, b){
+    let quotient = a / b;
+    return quotient;
+}
