4ba15edbcd09b9d33bd70549b01b963ce1a0e9d7
ewijah <edgordon2023@gmail.com>
2026-08-12 18:54:48 -0400

    add cs and user input

--- main/main.js
+++ main/main.js
@@ -1,6 +1,53 @@
 let num1 = 0;
 let num2 = 0;
-let operator;
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll(".calc-button");
+const clearButton = document.querySelector("#clear");
+let firstNumber = "";
+let operator = "";
+let secondNumber= "";
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        let value = button.textContent;
+
+        if (!isNaN(value)) {
+            if(display.textContent === "0") {
+                display.textContent = value;
+            } else{
+            display.textContent += value;
+             }
+
+             }
+             else if (value === "."){
+                if(!display.textContent.includes(".")) {
+                    display.textContent += "."
+                }
+
+        }
+        else if (value === "+" || value === "-" || value === "*" || value === "/" ){
+            firstNumber = Number(display.textContent);
+            operator = value;
+            display.textContent = "";
+        }
+
+        else if (value === "="){
+            secondNumber = Number(display.textContent);
+
+            let result = operate(operator, firstNumber, secondNumber);
+            display.textContent = result.toFixed(1);
+        }
+
+    });
+});
+
+clearButton.addEventListener("click", function() {
+    display.textContent = "0";
+    firstNumber = "";
+    operator = "";
+    secondNumber = "";
+
+})
 
 function add(a, b){
     let sum = a + b;
@@ -42,5 +89,5 @@ function operate(operator, num1, num2) {
 
     return outcome;
     
-
 }
+
